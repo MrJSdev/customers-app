9baaf960ad156eb7a8883aef219eb3a06210b3dd
MrJSdev <khanriyaz24@gmail.com>
2022-09-12 00:57:29 +0530

    fixed path of svg image

--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -2,7 +2,7 @@
   <q-page class="q-pa-lg bg-blue-1">
     <div class="text-h6 q-mb-md text-uppercase">Dashboard</div>
     <q-card class="q-mb-md q-pa-lg justify-center rounded-borders shadow-1 text-center">
-      <img src="public/images/welcome.svg" class="dashboard-page__welcome-image inline-block q-mb-lg" />
+      <img src="images/welcome.svg" class="dashboard-page__welcome-image inline-block q-mb-lg" />
       <div class="text-h6 q-mb-sm text-weight-bold">Welcome to Customers App</div>
       <p>Please check our customers list page as there is nothing on this dashboard page. Thank you🙂</p>
       <q-btn label="Customers" color="secondary" @click="$router.replace({ name: RoutesName.CUSTOMERS })"></q-btn>
